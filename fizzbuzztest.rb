13615e4e927bcf3bcbef07e8c46ff10236df23b3
GabbyMont <gemo1236@gmail.com>
2018-03-12 17:46:32 -0400

    first test: test_1_and_1

--- fizzbuzztest.rb
+++ fizzbuzztest.rb
@@ -8,4 +8,9 @@ class Tdd_fizzbuzz_test < Minitest::Test
 		assert_equal(true,true) # asserts that both items are equal to one another
 	end
 
+	def test_1_and_1
+		num1 = 1 # variable 'num1' is set equal to '1'
+		num2 = 1
+		assert_equal(num1,num2) #can be called or put into the assertion
+	end
 end
